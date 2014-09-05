623c04e14ae2a6e73c8165e8e2ef64c2a521c6c5
Saúl Díaz <sefford@gmail.com>
2014-09-05 13:12:04 +0200

    Fixed dependency upload stage

--- external-repo/upload.sh
+++ external-repo/upload.sh
@@ -6,4 +6,4 @@
 #
 
 #Upload Facebook to Local repo
-mvn install:install-file -Dfile=./support-v4-19.1.0.jar -DgroupId=com.android.support -DartifactId=support-v4 -Dversion=19.1.0
+mvn install:install-file -Dfile=./support-v4-19.1.0.jar -DgroupId=com.android.support -DartifactId=support-v4 -Dversion=19.1.0 -Dpackaging=jar
