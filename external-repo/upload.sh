51e159af8933b98666f1429411d2970e8c2a64f3
Saul Diaz <sefford@gmail.com>
2015-08-06 18:19:03 +0200

    First iteration to gradlelization

--- external-repo/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# ---------------------------------------------------------------------
-# Dependency upload
-# ---------------------------------------------------------------------
-#
-
-#Upload Facebook to Local repo
-mvn install:install-file -Dfile=./support-v4-19.1.0.jar -DgroupId=com.android.support -DartifactId=support-v4 -Dversion=19.1.0 -Dpackaging=jar
